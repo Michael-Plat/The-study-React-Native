6612a03cc02831f3b4354fd1b520765f3f6d26b4
Michael-Plat <67632735+Michael-Plat@users.noreply.github.com>
2021-08-25 02:18:44 +0300

    addet View, Text, Button, Alert, Image

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Button,
+  Alert,
+  TouchableWthoutFeedback,
+} from "react-native";
 
 export default function App() {
+  const handleTextPress = () => console.log("Text Pressed");
+  const handleButtonPress = () =>
+    Alert.alert("Ivan Oblomov", "Main message", [
+      { text: "Ok", onPress: () => console.log("Yes button") },
+      { text: "No", onPress: () => console.log("No button") },
+    ]);
+  const handleButtonPress2 = () =>
+    Alert.prompt("Ivan Oblomov", "Main message", (text) => console.log(text));
+
   return (
-    <View style={styles.container}>
-      <Text>Hello!!!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} style={styles.text} onPress={handleTextPress}>
+        Hello!!! {"\n"}Hello!!!
+      </Text>
+      <Button title="Нажми на меня" color="green" onPress={handleButtonPress} />
+      <Button
+        title="Нажми на меня"
+        color="green"
+        onPress={handleButtonPress2}
+      />
+      <TouchableWthoutFeedback>
+        <Image source={require("./assets/favicon.png")} />
+      </TouchableWthoutFeedback>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  text: {
+    color: "red",
   },
 });
